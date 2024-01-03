16b4fab328c471f3e93099e45a886ffa7d64ed2b
Samuel-EA1 <therealsamuelea@gmail.com>
2024-01-03 15:04:16 +0100

    made it responsive

--- index.js
+++ index.js
@@ -14,41 +14,58 @@ const kel_value = kel_Input;
 
 //Celsius to fahrenheit and Kelvin
 function cel_2_fah_kel(){
-    //celsius to fahrenheit
-   
-    const cel_value_num = Number(cel_Value.value);
-    const fah_result = cel_value_num * (9/5) + 32;
-    fah_Input.value = fah_result.toFixed(0);
-
-
-    //celsius to kelvin
-    const kel_result = cel_value_num + 273.15;
-    console.log(kel_result);
-    kel_Input.value = kel_result.toFixed(0);
-
-   
+   if (cel_Input.value === ""){
+        alert("Enter a number");
+        fah_Input.value = "";
+        kel_Input.value = "";
+   }
+   else {
+        //celsius to fahrenheit
+         const cel_value_num = Number(cel_Value.value);
+        const fah_result = cel_value_num * (9/5) + 32;
+        fah_Input.value = fah_result;
+          //celsius to kelvin
+         const kel_result = cel_value_num + 273.15;
+        console.log(kel_result);
+        kel_Input.value = kel_result;
+   }
 }
 
 function fah_2_cel_kel(){
-    //fahrenheit to celsius
-    const fah_value_num = Number(fah_value.value);
-    const cel_result = (fah_value_num - 32) * (5/9);
-    cel_Input.value = cel_result.toFixed(0);
+    if(fah_Input.value === ""){
+        alert("Enter a number");
+        cel_Input_Input.value = "";
+        kel_Input.value = "";
+    }
+    else {
+        //fahrenheit to celsius
+         const fah_value_num = Number(fah_value.value);
+        const cel_result = (fah_value_num - 32) * (5/9);
+        cel_Input.value = cel_result;
 
-    //fahrenheit to kelvin
-    const kel_result2 = (fah_value_num - 32) * (5/9) + 273.25;
-    kel_Input.value = kel_result2.toFixed(0);
+        //fahrenheit to kelvin
+        const kel_result2 = (fah_value_num - 32) * (5/9) + 273.25;
+        kel_Input.value = kel_result2;
+    }
+
 }
 
 function kel_2_cel_fah(){
-    //kelvin to celsius
-    const kel_value_num = Number(kel_value.value);
-    const cel_result2 = kel_value_num - 273.15;
-    cel_Input.value = cel_result2.toFixed(0);
+    if(kel_Input.value === ""){
+        alert("Enter a number");
+        cel_Input_Input.value = "";
+        fah_Input.value = "";
+    }
+    else {
+        //kelvin to celsius
+        const kel_value_num = Number(kel_value.value);
+        const cel_result2 = kel_value_num - 273.15;
+        cel_Input.value = cel_result2;
+        //kelvin to fahrenheit
+        const fah_result2 = (kel_value_num - 273.15) * (9/5) + 32;
+        fah_Input.value = fah_result2;
+    }
 
-    //kelvin to fahrenheit
-    const fah_result2 = (kel_value_num - 273.15) * (9/5) + 32;
-    fah_Input.value = fah_result2.toFixed(0);
 }
 
 //Clear result functions
